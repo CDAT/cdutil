16aacb6f8e8f5ebb0695309067afd80e66f2ac62
Denis Nadeau <dnadeau4@users.noreply.github.com>
2018-02-05 15:56:17 -0800

    can't trap stdout in circleci

--- tests/test_cdutil_selectRegion.py
+++ tests/test_cdutil_selectRegion.py
@@ -10,39 +10,14 @@ class CDUTIL(unittest.TestCase):
 
     def testRegions(self):
 
-        # ------------------------------------------------------
-        # Copy stdout and stderr file descriptor for cmor output
-        # ------------------------------------------------------
-        newstdout = os.dup(1)
-        newstderr = os.dup(2)
-        # --------------
-        # Create tmpfile
-        # --------------
-        tmpfile = tempfile.mkstemp()
-
-        os.dup2(tmpfile[0], 1)
-        os.dup2(tmpfile[0], 2)
-        os.close(tmpfile[0])
         regionNA = cdutil.region.domain(latitude=(-50.,50.,'ccb'))
         f=cdms2.open(cdat_info.get_sampledata_path()+'/clt.nc')
         d=f('u', regionNA)
 
-        os.dup2(newstdout, 1)
-        os.dup2(newstderr, 2)
-        sys.stdout = os.fdopen(newstdout, 'w', 0)
-        sys.stderr = os.fdopen(newstderr, 'w', 0)
         # --------------------------------------------------------
         # makesure the warning has been displayed for the 3rd args
         # --------------------------------------------------------
-        f = open(tmpfile[1], 'r')
-        lines = f.readlines()
-        for line in lines:
-            if line.find('Only axis') != -1:
-                break
-        f.close()
-        os.unlink(tmpfile[1])
 
-        self.assertIn('Only axis', line.strip())
         bounds = d.getLatitude().getBounds()
         self.assertTrue(numpy.allclose(bounds[0], numpy.array([-50., -49.19124603])))
         self.assertTrue(numpy.allclose(bounds[-1], numpy.array([49.19124603,  50.])))
