d2d4cbf05e8dac9c174a5c49bbc6b23a4964acc2
Muryanto <muryanto1@ML-9516678.llnl.gov>
2018-07-17 08:37:28 -0700

    use TestRunnerBase

--- run_tests.py
+++ run_tests.py
@@ -1,38 +1,13 @@
-import sys
+#!/usr/bin/env python
 import os
-import argparse
-import multiprocessing
-
-#                                                                                                                                    
-# TEMPORARY -- REVISIT                                                                                                               
-#                                                                                                                                    
-#sys.path.append("/Users/muryanto1/repos/cdat_info/Lib")
-
-from cdat_info.Lib.Util import *
-from cdat_info.Lib.Const import *
-
-import cdat_info.Lib.TestRunnerBase
+import sys
+import cdat_info
 
 test_suite_name = 'cdutil'
 
-parser = argparse.ArgumentParser(description="Run {n} tests".format(n=test_suite_name),
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-valid_options = 0x00000000
-valid_options |= OPT_GENERATE_HTML
-valid_options |= OPT_PACKAGE_RESULT
-valid_options |= OPT_FAILED_ONLY
-valid_options |= OPT_VERBOSITY
-valid_options |= OPT_NCPUS
-add_arguments(parser, valid_options)
-
-args = parser.parse_args()
 workdir = os.getcwd()
-get_sample_data = True
+runner = cdat_info.TestRunnerBase(test_suite_name, get_sample_data=True)
+ret_code = runner.run(workdir)
 
-runner = TestRunnerBase.TestRunnerBase(test_suite_name, valid_options, args, get_sample_data)
-runner.run(workdir, args.tests)
-
-
-
+sys.exit(ret_code)
 
