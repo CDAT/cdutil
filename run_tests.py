5195be2f726350a025990558f242470cfdc1c6f2
Muryanto <muryanto1@ML-9516678.llnl.gov>
2018-08-17 15:06:24 -0700

    workdir should be the directory where run_tests.py is

--- run_tests.py
+++ run_tests.py
@@ -5,7 +5,7 @@ import cdat_info
 
 test_suite_name = 'cdutil'
 
-workdir = os.getcwd()
+workdir = os.path.dirname(os.path.realpath(__file__))
 runner = cdat_info.TestRunnerBase(test_suite_name, get_sample_data=True)
 ret_code = runner.run(workdir)
 
