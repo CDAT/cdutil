2a010c0b8cd25f092f7fc4559e1fc44ef673a14e
Muryanto <muryanto1@ML-9516678.llnl.gov>
2018-08-17 15:31:10 -0700

    try add path to checkout

--- run_tests.py
+++ run_tests.py
@@ -5,7 +5,7 @@ import cdat_info
 
 test_suite_name = 'cdutil'
 
-workdir = os.path.dirname(os.path.realpath(__file__))
+workdir = os.getcwd()
 runner = cdat_info.TestRunnerBase(test_suite_name, get_sample_data=True)
 ret_code = runner.run(workdir)
 
