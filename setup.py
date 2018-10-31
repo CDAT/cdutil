dc9539089eeb721da29562155d86dbdfbe6bdeb3
Charles Doutriaux <doutriaux1@llnl.gov>
2018-10-31 08:40:07 -0700

    switch to setupttool/egg and noarch

--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+from subprocess import Popen, PIPE
+
+
+Version = "8.0"
+p = Popen(
+    ("git",
+     "describe",
+     "--tags"),
+    stdin=PIPE,
+    stdout=PIPE,
+    stderr=PIPE)
 try:
-    sys.path.append(os.environ['BUILD_DIR'])
-    import cdat_info
-    Version=cdat_info.Version
+    descr = p.stdout.readlines()[0].strip().decode("utf-8")
+    Version = "-".join(descr.split("-")[:-2])
+    if Version == "":
+        Version = descr
 except:
-    Version="???"
+    descr = Version
 
 setup (name = "cdutil",
-       author="PCMDI Software Team",
-       version=Version,
+       author="AIMS Software Team",
+       version=descr,
        description = "Utilities for climate data manipulation",
        url = "http://cdat.sourceforge.net",
-       packages = ['cdutil'],
-       package_dir = {'cdutil': 'Lib'},
+       packages = find_packages(),
        data_files = [ ("share/cdutil",("data/sftbyrgn.nc","data/navy_land.nc","share/test_data_files.txt"))],
       )
     
